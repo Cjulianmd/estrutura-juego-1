309970c3e7ba5c37e89cab3a97ba573a2e9e665c
AlhanRendon <alhanrendon@gmail.com>
2024-09-25 10:02:56 -0500

    commit

--- Clases.cpp
+++ Clases.cpp
@@ -1,8 +1,61 @@
+#include <stack>
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class carta{
+    public:
+        char tipo;
+        char pinta;
+        carta(char tipo='n', char pinta = 'a'){
+            this->tipo = tipo;
+            this->pinta = pinta;
+        }
+        void Mostrar(){
+            cout << "(" << tipo << "," << pinta << ") ";     
+        };
+};
+
+class baraja{
+    public:
+    stack<int> recibir;
+    int darcarta(){
+        if(!recibir.empty()){
+            int c = recibir.top();
+            recibir.pop();
+            return c;
+        }
+        int c = -1;
+        return c;
+    };
+};
+
+class jugador{
+    public:
+        int mano[9];
+        void obtenerCarta(baraja* mazo){
+            int c = mazo->darcarta();
+            mano[c]++;
+            
+        };  
+};
+
 
 int main() {
-    // Write C++ code here
-    std::cout << "alhan rendon";
+    baraja B;
+    jugador J;
+    for(int i =0; i<10;i++){
+    J.mano[i] = 0;
+    }
+
+    vector<int> columna = {};
+    for (auto i : columna) {
+    J.mano[i]++;
+    }
+    cout<< " " << endl;
+    for(int i=0;i<10;i++){
+    cout<< J.mano[i] << " ";
+    }
 
     return 0;
 }
