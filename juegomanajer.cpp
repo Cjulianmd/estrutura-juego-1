a89b4b25d6ba6e21c0a5a372b2a4a82201e26883
AlhanRendon <alhanrendon@gmail.com>
2024-09-27 16:06:45 -0500

    Update juegomanajer.cpp

--- juegomanajer.cpp
+++ juegomanajer.cpp
@@ -4,6 +4,7 @@ void iniciarJuego() {
     int numJugadores;
     std::cout << "Ingrese el número de jugadores (3-5): ";
     std::cin >> numJugadores;
+    // este dato sirve para si se desea jugar varias partidas seguido y que todas sumen puntos al final
     int numPartidas = 1;
 
     Colorette juego(numJugadores, numPartidas);
